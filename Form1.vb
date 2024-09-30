699cd6b9bd500d054f3e2bee07785a1434c28f01
MUSIMBI FRANCIS <128372690+Hellysisco@users.noreply.github.com>
2024-09-30 09:19:35 +0300

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        'Get use input and convert it to a number
+        Dim enterNumber As Double
+
+        'Check if the input is a valid number
+        If Double.TryParse(txtEnterNumber.Text, enterNumber) Then
+            'Conditional statements to check if the number is positive, negative, or zero
+            If enterNumber > 0 Then
+                lblResult.Text = "Positive Number"
+            ElseIf enterNumber < 0 Then
+                lblResult.Text = "Negative Number"
+            Else
+                lblResult.Text = "Zero"
+            End If
+        Else
+            'Handle invalid iput
+            lblResult.Text = "Please enter a valid number."
+        End If
+    End Sub
+End Class
